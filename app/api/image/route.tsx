a59463073cc0085b61039661e9b59d3228df3705
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-03-23 21:42:53 +0000

    test

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -1,22 +1,83 @@
 import { NextRequest } from 'next/server'
 
-// import { ImageResponse } from "@vercel/og";
+import { ImageResponse } from "@vercel/og";
 
 export const dynamic = 'error'
 export const revalidate = 86400
 export const runtime = 'edge'
 
-export const FRAME_IMAGE_DIMENSIONS = {
+const FRAME_IMAGE_DIMENSIONS = {
     width: 1200,
     height: 630,
 }
 
+const generateFrameImage = async (content: React.ReactNode) => {
+    const imageResponse = new ImageResponse(
+      (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "stretch",
+            width: "100%",
+            height: "100vh",
+            backgroundColor: "white",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              lineHeight: 1.2,
+              fontSize: 36,
+              color: "black",
+              flex: 1,
+              overflow: "hidden",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ),
+      FRAME_IMAGE_DIMENSIONS
+    )
+    return imageResponse
+}
+
 export async function GET(_request: NextRequest, { params }: { params: { index: number } }) {
     const images = [
-        'https://i.ytimg.com/vi/j1FkGgTvwvU/maxresdefault.jpg'
+        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+            <div tw="flex w-full h-full text-white bg-blue">
+                <div tw="flex flex-row w-full p-15 items-center justify-center">
+                    <h2 tw="flex flex-col text-8xl font-bold tracking-tight text-left px-10">
+                        <span>This is an example dynamic image</span>
+                    </h2>
+                </div>
+            </div>
+        </div>,
+        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+            <div tw="flex w-full h-full text-white bg-blue">
+                <div tw="flex flex-row w-full p-15 items-center justify-center">
+                    <h2 tw="flex flex-col text-8xl font-bold tracking-tight text-left px-10">
+                        <span>This is another example dynamic image</span>
+                    </h2>
+                </div>
+            </div>
+        </div>,
+        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+            <div tw="flex w-full h-full text-white bg-blue">
+                <div tw="flex flex-row w-full p-15 items-center justify-center">
+                    <h2 tw="flex flex-col text-8xl font-bold tracking-tight text-left px-10">
+                        <span>This is another another example dynamic image</span>
+                    </h2>
+                </div>
+            </div>
+        </div>,
     ]
 
-    return (
+    return generateFrameImage(
         images[params.index]
     )
 }
